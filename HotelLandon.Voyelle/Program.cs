e13b343bf295a88ef59ef7cc9cd92483293c9fae
Andres Talavera <andres.talavera@live.fr>
2020-02-27 12:20:14 +0100

    Add Voyelle project

---
+++ HotelLandon.Voyelle/Program.cs
@@ -0,0 +1,21 @@
+﻿using HotelLandon.Models;
+using System;
+
+namespace HotelLandon.Voyelle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int indexFloor = 1; indexFloor <= 10; indexFloor++)
+            {
+                for (int indexRoomNumber = 0; indexRoomNumber < 10; indexRoomNumber++)
+                {
+                    int roomNumber = indexFloor * 100 + indexRoomNumber;
+                    Room roomVariable = new Room(indexFloor, roomNumber);
+                    Console.WriteLine("Etage:" + roomVariable.Floor + " Chambre: " + roomVariable.Number);
+                }
+            }
+        }
+    }
+}
