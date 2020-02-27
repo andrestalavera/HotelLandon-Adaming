f22e5eed0b21466eee26d1f96ddb3e23c88e9c82
Andres Talavera <andres.talavera@live.fr>
2020-02-27 11:08:02 +0100

    Add models

---
+++ HotelLandon.Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HotelLandon.Models
+{
+    public class Customer
+    {
+        public int      Id          { get; set; }
+        public bool     IsFemale    { get; set; }
+        public string   FirstName   { get; set; }
+        public string   LastName    { get; set; }
+        public DateTime BirthDate   { get; set; }
+    }
+}
---
+++ HotelLandon.Models/Room.cs
@@ -0,0 +1,43 @@
+﻿namespace HotelLandon.Models
+{
+    public class Room
+    {
+        #region Mes propriétés
+        /* portée type     Nom         get/set */
+        public int      Number      { get; set; }
+        public int      Floor       { get; set; }
+        public object   Standing    { get; set; }
+        public bool     IsCleaned   { get; set; }
+        public bool     IsBooked    { get; set; }
+        #endregion
+
+        #region Constructeurs
+        /// <summary>
+        /// Contruit une chambre à l'aide du numéro d'étage et de son numéro
+        /// </summary>
+        /// <param name="floor">Numéro d'étage</param>
+        /// <param name="number">Numéro de chambre</param>
+        public Room(int floor, int number)
+        {
+            Floor = floor;
+            Number = number;
+        }
+
+        /// <summary>
+        /// Construit une chambre à l'aide du numéro d'étage, de son numéro, du standing et défini si elle est propre ou réservée
+        /// </summary>
+        /// <param name="floor">Numéro d'étage</param>
+        /// <param name="number">Numéro de chambre</param>
+        /// <param name="standing">Gamme de la chambre</param>
+        /// <param name="isCleaned">Est-ce que la chambre est propre ?</param>
+        /// <param name="isBooked">Est-ce que la chambre est réservée ?</param>
+        public Room(int floor, int number, object standing, bool isCleaned, bool isBooked) 
+            : this(floor, number)
+        {
+            Standing = standing;
+            IsCleaned = isCleaned;
+            IsBooked = isBooked;
+        }
+        #endregion
+    }
+}
