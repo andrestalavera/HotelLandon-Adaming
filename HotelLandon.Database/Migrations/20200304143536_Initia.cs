0745fb02a3f058292eb24e7ff90237a66c0943b6
Andres Talavera <andres.talavera@live.fr>
2020-03-04 16:43:36 +0100

    First steps with linq

---
+++ HotelLandon.Database/Migrations/20200304143536_Initia.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelLandon.Database.Migrations
+{
+    public partial class Initia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Start = table.Column<DateTime>(nullable: false),
+                    End = table.Column<DateTime>(nullable: false),
+                    CustomerId = table.Column<int>(nullable: false),
+                    RoomNumber = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Number = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberRoom = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Number);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
--- HotelLandon.Database/Program.cs
+++ HotelLandon.Database/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HotelLandon.Database
 {
@@ -6,7 +10,90 @@ namespace HotelLandon.Database
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Hello Database");
+            HotelContext context = new HotelContext();
+            //Customer customer = new Customer()
+            //{
+            //    FirstName = "Andres",
+            //    LastName = "Talavera",
+            //    BirthDate = new DateTime(1992, 10, 29)
+            //};
+            //context.Customers.Add(customer);
+            //int changedElements =  context.SaveChanges();
+            //Console.WriteLine(changedElements + " ont changé");
 
+            Console.WriteLine("Ajout des chambres");
+            List<Room> rooms = new List<Room>();
+            for (int indexFloor = 1; indexFloor <= 10; indexFloor++)
+            {
+                for (int indexRoomNumber = 0; indexRoomNumber < 10; indexRoomNumber++)
+                {
+                    int roomNumber = indexFloor * 100 + indexRoomNumber;
+                    Room room1 = new Room()
+                    {
+                        NumberRoom = roomNumber
+                    };
+                    rooms.Add(room1);
+                }
+            }
+            context.Rooms.AddRange(rooms);
+            //context.SaveChanges();
+
+
+            // Affichage des chambres
+            Console.WriteLine("Affichage des chambres avec linq to sql");
+            IQueryable<Room> orderedRooms = from r 
+                                            in context.Rooms
+                                            orderby r.NumberRoom
+                                            select r;
+            List<Room> finalRooms = orderedRooms.ToList();
+            foreach (var room in finalRooms)
+            {
+                Console.WriteLine($"Id : {room.Number} / N° : {room.NumberRoom}");
+            }
+
+            Console.WriteLine("Affichage avec Linq (System.Linq)");
+
+            foreach (Room room in context.Rooms.OrderBy(r => r.NumberRoom).ToList())
+            {
+                Console.WriteLine($"Id : {room.Number} / N° : {room.NumberRoom}");
+            }
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+
+    public class Room
+    {
+        [Key]
+        public int Number { get; set; }
+        public int NumberRoom { get; set; }
+    }
+
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int CustomerId { get; set; }
+        public int RoomNumber { get; set; }
+    }
+
+    public class HotelContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=HotelLandon;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
 }
